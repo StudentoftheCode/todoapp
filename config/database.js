888cd6b177bfe3cdc6f0058b6fe026a65e2b6b85
Chris Kennedy <christopher.kennedy0312@gmail.com>
2025-11-08 22:41:58 -0500

    deploy to heroku

--- config/database.js
+++ config/database.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error('Error connecting to MongoDB:', err);
